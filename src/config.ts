eb1665c3d26f15ccdc5997121ef502709e0bc856
aiwe <maksymivv@gmail.com>
2022-07-19 16:56:12 +0300

    Add two native nodes

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,8 @@ let global : any = typeof window !== 'undefined' ? window : self;
 global.config = {
     nodeList: [
         "https://karbo.club/services/node_web/",
-		"https://node.karbo.org:32448/"
+		"https://node.karbo.org:32448/",
+		"https://node2.karbo.org:32448/"
     ],
 	nodeUrl: "",
 	mainnetExplorerUrl: "http://explorer.karbowanec.com/",
