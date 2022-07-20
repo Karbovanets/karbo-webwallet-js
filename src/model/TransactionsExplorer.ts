24bb63dd0a9f3695ff9ab02cdd3dda611212fa43
aiwe <maksymivv@gmail.com>
2022-07-20 18:47:05 +0300

    Maybe fix isMinerTx

--- src/model/TransactionsExplorer.ts
+++ src/model/TransactionsExplorer.ts
@@ -131,21 +131,11 @@ export class TransactionsExplorer {
 	}
 
 	static isMinerTx(rawTransaction: RawDaemon_Transaction) {
-		if (!Array.isArray(rawTransaction.vout) || rawTransaction.vin.length > 0) {
-			return false;
+		if(rawTransaction.vin.length > 0 && rawTransaction.vin[0].type === 'ff') {
+			return true;
 		}
 
-		if (!Array.isArray(rawTransaction.vout) || rawTransaction.vout.length === 0) {
-			console.error('Weird tx !', rawTransaction);
-
-			return false;
-		}
-
-		try {
-			return rawTransaction.vout[0].amount !== 0;
-		} catch (err) {
-			return false;
-		}
+		return false;
 	}
 
 	static parse(rawTransaction: RawDaemon_Transaction, wallet: Wallet): Transaction | null {
