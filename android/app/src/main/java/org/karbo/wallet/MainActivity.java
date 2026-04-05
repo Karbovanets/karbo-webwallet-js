493c6073cd998aac68e8acbfa86c3ebace385f83
aiwe <maksymivv@gmail.com>
2026-04-05 15:50:54 -0500

    Set status/navigation bar colors programmatically in MainActivity

--- android/app/src/main/java/org/karbo/wallet/MainActivity.java
+++ android/app/src/main/java/org/karbo/wallet/MainActivity.java
@@ -1,5 +1,26 @@
 package org.karbo.wallet;
 
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Window window = getWindow();
+        window.setStatusBarColor(Color.parseColor("#111827"));
+        window.setNavigationBarColor(Color.parseColor("#111827"));
+
+        // Light status bar icons (white) on dark background
+        View decorView = window.getDecorView();
+        int flags = decorView.getSystemUiVisibility();
+        flags &= ~View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+        flags &= ~View.SYSTEM_UI_FLAG_LIGHT_NAVIGATION_BAR;
+        decorView.setSystemUiVisibility(flags);
+    }
+}
